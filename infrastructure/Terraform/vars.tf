d0f8c1914fa3307a1df1b0ecfc49798402490c3e
phu tran <tran.ph@northeastern.edu>
2021-03-24 19:08:27 -0700

    rework

---
+++ infrastructure/Terraform/vars.tf
@@ -0,0 +1,25 @@
+variable "AWS_REGION" {
+}
+
+variable "AMI" {
+}
+
+variable "DB_USERNAME"{
+
+}
+
+variable "DB_PASSWORD"{
+
+}
+
+variable "BUCKETNAME"{
+
+}
+
+variable "zone_id"{
+
+}
+
+variable "domain_name"{
+    
+}
